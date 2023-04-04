9dff729c6096c8a590cb87b8081ba385676176de
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-05 06:47:57 +0900

    ユーザー情報削除機能追加

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
 
 //ユーザー削除
-//ユーザー情報の更新
-//ユーザー専用編集ページ取得
+router.delete("/:id", async (req, res) => {
+  const user = await User.findById({ _id: req.params.id });
+  const { id, password } = req.body;
+  const compared = await bcrypt.compare(password, user.password);
+  try {
+    if (user.id === id && compared) {
+      await User.deleteOne({ _id: id });
+      await Post.deleteMany({ userId: id });
+      return res
+        .status(200)
+        .json("ユーザー情報、これまでの投稿を削除しました。");
+    } else {
+      return res.status(400).json("パスワードとユーザー情報が一致しません");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
 module.exports = router;
