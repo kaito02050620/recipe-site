621adf4c18f4809cb3e9b3f5d0ce47cca934d611
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-05 06:47:17 +0900

    投稿機能修正

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -12,24 +12,62 @@ router.post("/", async (req, res) => {
   }
 });
 
-//レシピを削除
-router.delete("/:id", async (req, res) => {
-  const recipe = await Post.findById(req.params.id);
-  const currentUserId = req.body._id;
-  console.log(recipe);
+//レシピを全て取得
+router.get("/allRecipe", async (req, res) => {
   try {
-    if (recipeId.userId === currentUserId)
-      return res.status(200).json("レシピを削除しました");
+    const recipes = await Post.find();
+    return res.status(200).json(recipes);
   } catch (error) {
     return res.status(500).json(error);
   }
 });
-//レシピを取得　状態管理しているIDとレシピUserIｄの一致確認
+
+//レシピを削除
+router.delete("/:id", async (req, res) => {
+  const recipe = await Post.findById(req.params.id);
+  const currentUserId = req.body.id;
+  try {
+    if (recipe.userId === currentUserId) {
+      await Post.deleteOne({ id: req.params.id });
+      return res.status(200).json("レシピを削除しました");
+    } else {
+      return res
+        .status(400)
+        .json("自身の投稿したレシピではない為、削除出来ません");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
 //レシピを更新
-//レシピを全て取得
+router.put("/:id", async (req, res) => {
+  const recipe = await Post.findById(req.params.id);
+  const currentUserId = req.body.userId;
+  try {
+    if (recipe.userId === currentUserId) {
+      const newRecipe = await Post.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      return res.status(200).json(newRecipe);
+    } else {
+      return res
+        .status(400)
+        .json("自身の投稿したレシピではない為、編集出来ません");
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 //特定のユーザーの投稿のみ取得
-//特定レシピのカテゴリを全て取得
-//特定レシピのカテゴリを取得
+router.get("/:id", async (req, res) => {
+  try {
+    const recipes = await Post.find({ userId: req.params.id });
+    return res.status(200).json(recipes);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
 module.exports = router;
