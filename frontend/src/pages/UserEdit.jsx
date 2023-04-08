d72d723e8a4813ca1f9472727e7bb5cd8ee48141
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-08 21:07:21 +0900

    ユーザー編集画面作成開始

---
+++ frontend/src/pages/UserEdit.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//投稿したレシピ一覧表示（投稿日、レシピ名、以下ボタン）
+//投稿したレシピ削除ボタン
+//投稿したレシピ更新ボタン
+//
+
+function User() {
+  return (
+    <div className="sectionBoard py-16">
+      <h1 className="text-3xl text-center mb-16">個人ページ（ﾀｹﾉｺ）</h1>
+    </div>
+  );
+}
+
+export default User;
