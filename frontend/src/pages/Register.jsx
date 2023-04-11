3e6d08bd8b1aca2737cd996f783ff996b922594a
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-11 23:49:08 +0900

    ユーザー登録API連携開始

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
+const API_SERVER = import.meta.env.VITE_API_SERVER;
+
+console.log(API_SERVER + "/auth/register");
 
 function Register() {
+  const userName = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const confirmPassword = useRef(null);
+
+  const newRegisterButton = async (e) => {
+    e.preventDefault();
+    if (
+      userName.current.value === "" &&
+      email.current.value === "" &&
+      password.current.value === "" &&
+      confirmPassword.current.value === ""
+    ) {
+      if (password.current.value === confirmPassword.current.value) {
+        try {
+          await axios.post(API_SERVER + "/auth/register", {
+            username: userName.current.value,
+            email: email.current.value,
+            password: password.current.value,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        confirmPassword.current.setCustomValidity(
+          "パスワードが一致していません"
+        );
+        password.current.value = "";
+        confirmPassword.current.value = "";
+      }
+    } else {
+      console.log("登録できません");
+    }
+  };
+
   return (
     <div className="sectionBoard w-full p-20 ">
       <div className="max-w-sm bg-white bg-opacity-80 rounded-sm shadow-md p-6 m-auto ">
@@ -16,6 +55,9 @@ function Register() {
               name="username"
               id="username"
               placeholder="ユーザー名"
+              required
+              min={1}
+              ref={userName}
             />
           </div>
           <div className="mb-6">
@@ -28,6 +70,9 @@ function Register() {
               name="email"
               id="email"
               placeholder="メールアドレス"
+              required
+              ref={email}
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"
             />
           </div>
           <div className="mb-6">
@@ -40,25 +85,32 @@ function Register() {
               name="password"
               id="password"
               placeholder="パスワード"
+              required
+              ref={password}
+              minLength="6"
             />
           </div>
           <div className="mb-6">
-            <label htmlFor="confirmationPassword" className="block mb-1">
+            <label htmlFor="confirmPassword" className="block mb-1">
               確認用パスワード
             </label>
             <input
               className="block w-full p-2 border-gray-500 border-solid"
               type="password"
-              name="confirmationPassword"
+              name="confirmPassword"
               id="confirmationPassword"
               placeholder="確認用パスワード"
+              required
+              ref={confirmPassword}
+              minLength="6"
             />
           </div>
-          <input
+          <button
+            onClick={(e) => newRegisterButton(e)}
             type="submit"
             value="新規登録する"
             className="block bg-red-300 bg-opacity-40 px-4 py-2 rounded-sm border-solid border-gray-800 border m-auto"
-          />
+          ></button>
         </form>
       </div>
     </div>
