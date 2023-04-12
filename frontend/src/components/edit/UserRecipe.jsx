2ceb64cb016fa271ea7abd7b9f3dce7fad488e2e
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-12 23:50:02 +0900

    投稿削除機能、データベース連携

--- frontend/src/components/edit/UserRecipe.jsx
+++ frontend/src/components/edit/UserRecipe.jsx
@@ -1,21 +1,41 @@
-import React from "react";
-import seafoodImage from "../../../public/assets/image/seafood_sample.png";
+import axios from "axios";
+import React, { useEffect } from "react";
+const API_SERVER = import.meta.env.VITE_API_SERVER;
+const TEST_USER = import.meta.env.VITE_TEST_USER;
 
-function UserRecipe() {
+function UserRecipe({ ...recipe }) {
+  //日付変換
+  const date = new Date(recipe.updatedAt);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const newUpdatedAt = `${year}/${month}/${day}`;
+
+  const deleteButton = async () => {
+    await axios.delete(API_SERVER + "/posts/" + recipe._id, {
+      data: {
+        id: TEST_USER,
+      },
+    });
+  };
+
   return (
     <li className="md:flex flex-wrap w-full p-2 mb-2 items-center bg-orange-200">
       <div className="sm:w-3/5 sm:flex items-center justify-between">
-        <p className="mr-10">ちりめんたっぷり海鮮丼 ~大トロを添えて~</p>
-        <img src={seafoodImage} alt="" className="h-24 object-cover" />
+        <p className="mr-10">{recipe.title}</p>
+        <img src={recipe.image} alt="" className="h-24 object-cover" />
       </div>
       <div className="w-1/5">
-        <p className="w-full text-center">2000/12/30</p>
+        <p className="w-full text-center">{newUpdatedAt}</p>
       </div>
       <div className="w-1/5">
         <button className="bg-green-300 bg-opacity-40 rounded-sm border-solid border-gray-800 border inline-block w-12 h-10 mr-2">
           編集
         </button>
-        <button className="bg-red-300 bg-opacity-40 rounded-sm border-solid border-gray-800 border inline-block w-12 h-10">
+        <button
+          className="bg-red-300 bg-opacity-40 rounded-sm border-solid border-gray-800 border inline-block w-12 h-10"
+          onClick={deleteButton}
+        >
           削除
         </button>
       </div>
