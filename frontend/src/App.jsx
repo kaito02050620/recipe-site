d92c762ff3344f690acfa645c3b87dbb98111210
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-12 23:55:27 +0900

    画面マウント時にページトップへ移動

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ScrollTop from "./location/ScrollTop";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Home from "./pages/Home";
@@ -6,17 +8,18 @@ import Developer from "./pages/Developer";
 import SiteDescription from "./pages/SiteDescription";
 import RecipeLists from "./pages/RecipeList";
 import Register from "./pages/Register";
-import { Routes, Route } from "react-router-dom";
-import Recipe from "./components/list/Recipe";
+import Recipe from "./pages/Recipe";
 import Login from "./pages/Login";
 import User from "./pages/UserEdit";
+const TEST_USER = import.meta.env.VITE_TEST_USER;
 
 const App = () => {
   return (
     <>
-      <div className="bg-[url('./assets/image/background.png')] bg-cover w-full h-full">
+      <div className="bg-[url('./image/background.png')] bg-cover w-full h-full">
         <Header />
         <main className="max-w-screen-xl mx-auto mt-3 mb-52 px-8">
+          <ScrollTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/developer" element={<Developer />} />
@@ -26,7 +29,7 @@ const App = () => {
             <Route path="/recipe/:id" element={<Recipe />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/user:username" element={<User />} />
+            <Route path={`/user/${TEST_USER}`} element={<User />} />
           </Routes>
         </main>
         <Footer />
