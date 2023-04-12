9b0369f9e56391e9ca1372316bba98bd38e70b55
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-12 23:50:54 +0900

    夕飯表示作成

--- frontend/src/components/home/Dinner.jsx
+++ frontend/src/components/home/Dinner.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import seafoodImage from "../../../public/assets/image/seafood_sample.png";
+import { recipeName } from "../select/select";
 
 function Dinner() {
+  //ランダムにレシピを取得する
+  const [abstract, ...other] = recipeName;
+  const randomRecipe = other[Math.floor(Math.random() * other.length)];
+
+  const [cook, setCook] = useState(randomRecipe);
+
+  function changeRecipe(e) {
+    e.preventDefault;
+    const randomRecipe = other[Math.floor(Math.random() * other.length)];
+    setCook(randomRecipe);
+  }
+
   return (
     <>
       <div className="sectionBoard w-3/5 p-5">
         <h1 className="text-3xl mb-10">今夜の晩御飯</h1>
         <div className="flex items-center justify-end">
           <div className="w-full text-center">
-            <p className="text-5xl mb-8">海鮮丼</p>
+            <p className="text-5xl mb-8">{cook}</p>
+            <button onClick={(e) => changeRecipe(e)}>夕飯を変更する</button>
             <Link className="viewRecipeButton" to="/recipeList">
               レシピを見る
               <svg
